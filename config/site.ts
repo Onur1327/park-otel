c4b32d33f3d7b6d5224ef1f36982d3c498db50dd
Onur1327 <onurlacin1995@gmail.com>
2026-01-21 13:24:23 +0300

    Tum Rezervasyon Yap butonlari trivago linkine yonlendirildi

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,7 @@
 // Site yapılandırma dosyası
 export const siteConfig = {
-  // Rezervasyon linki - Buraya rezervasyon sayfanızın URL'sini ekleyin
-  reservationUrl: process.env.NEXT_PUBLIC_RESERVATION_URL || '#',
+  // Rezervasyon linki - tüm "Rezervasyon Yap" butonları buraya yönlenecek
+  reservationUrl:
+    'https://www.trivago.com.tr/tr/oar/kayseri-park-hotel?search=100-1095636&sem_creativeid=599847500055&sem_network=g&sem_device=c&sem_target=&sem_adposition=&sem_campaignid=372516742&sem_adgroupid=22532945182&sem_targetid=dsa-58668777886&sem_location=9056710&cipc=sem&cip=901131&gad_source=1&gad_campaignid=372516742&gbraid=0AAAAAD6-gN84uooVkRYhwhbvz_AhA6ja9&gclid=CjwKCAiAj8LLBhAkEiwAJjbY7_hLogNAebQuXb-8kX4SFhtglo5RspqvolyoTr8Nz59FKuvaMrG6YxoCCDkQAvD_BwE',
 }
 
--- data/hotels.ts
+++ data/hotels.ts
@@ -35,7 +35,8 @@ export const HOTELS: StaticHotel[] = [
       'Toplantı Alanı',
     ],
     location: 'Şişli, İstanbul',
-    bookingLink: 'https://www.orneksite.com/rezervasyon/hotel-1',
+    bookingLink:
+      'https://www.trivago.com.tr/tr/oar/kayseri-park-hotel?search=100-1095636&sem_creativeid=599847500055&sem_network=g&sem_device=c&sem_target=&sem_adposition=&sem_campaignid=372516742&sem_adgroupid=22532945182&sem_targetid=dsa-58668777886&sem_location=9056710&cipc=sem&cip=901131&gad_source=1&gad_campaignid=372516742&gbraid=0AAAAAD6-gN84uooVkRYhwhbvz_AhA6ja9&gclid=CjwKCAiAj8LLBhAkEiwAJjbY7_hLogNAebQuXb-8kX4SFhtglo5RspqvolyoTr8Nz59FKuvaMrG6YxoCCDkQAvD_BwE',
     featured: true,
     contact: {
       phone: '+90 (212) 123 45 67',
@@ -54,7 +55,8 @@ export const HOTELS: StaticHotel[] = [
     ],
     amenities: ['İnternet', 'Otopark', 'Restoran', '24/7 Güvenlik'],
     location: 'Kadıköy, İstanbul',
-    bookingLink: 'https://www.orneksite.com/rezervasyon/hotel-2',
+    bookingLink:
+      'https://www.trivago.com.tr/tr/oar/kayseri-park-hotel?search=100-1095636&sem_creativeid=599847500055&sem_network=g&sem_device=c&sem_target=&sem_adposition=&sem_campaignid=372516742&sem_adgroupid=22532945182&sem_targetid=dsa-58668777886&sem_location=9056710&cipc=sem&cip=901131&gad_source=1&gad_campaignid=372516742&gbraid=0AAAAAD6-gN84uooVkRYhwhbvz_AhA6ja9&gclid=CjwKCAiAj8LLBhAkEiwAJjbY7_hLogNAebQuXb-8kX4SFhtglo5RspqvolyoTr8Nz59FKuvaMrG6YxoCCDkQAvD_BwE',
     featured: false,
     contact: {
       phone: '+90 (212) 123 45 67',
